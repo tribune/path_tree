27de615a04285ca287e4da0776f530af0cb4617d
kliu <kliu@tribpub.com>
2015-06-11 16:17:33 -0500

    rspecs

--- spec/path_tree_spec.rb
+++ spec/path_tree_spec.rb
@@ -35,14 +35,49 @@ describe PathTree do
   end
 
   context "validations" do
-    it "is invalid without a name"
-    it "is invalid without a node_path"
-    it "is invalid without a path"
+    let(:new_rec) { PathTree::Test.new }
 
-    it "enforces unique path"
+    it "is invalid without a name" do
+      expect(new_rec).not_to be_valid
+      expect(new_rec.errors[:name].first).to match /can't be blank/
+    end
 
-    it "enforces unique node_path under a parent"
-    it "allows duplicate node_path under different parents"
+    it "is invalid without a node_path" do
+      new_rec.name = "I'm a leaf"
+      new_rec.node_path = ''
+      expect(new_rec).not_to be_valid
+      expect(new_rec.errors[:node_path].first).to match /can't be blank/
+    end
+
+    it "is invalid without a path" do
+      new_rec.name = "I'm a leaf"
+      new_rec.path = ''
+      expect(new_rec).not_to be_valid
+      expect(new_rec.errors[:path].first).to match /can't be blank/
+    end
+
+    context "uniqueness" do
+      around :each do |ex|
+        PathTree::Test.transaction do
+          @root = PathTree::Test.create!(name: 'top-dog')
+          @child_1 = PathTree::Test.create!(name: 'underdog', parent: @root)
+          ex.run
+          raise ActiveRecord::Rollback
+        end
+      end
+
+      it "enforces unique path" do
+        new_rec = PathTree::Test.new(name: @child_1.node_path, parent: @root)
+        expect(new_rec).not_to be_valid
+        expect(new_rec.errors[:path]).to include 'has already been taken'
+        expect(new_rec.errors[:node_path]).to include 'has already been taken'
+      end
+
+      it "allows duplicate node_path under different parents" do
+        new_rec = PathTree::Test.new(name: @child_1.node_path)
+        expect(new_rec).to be_valid
+      end
+    end
   end
   
   context "path construction" do
